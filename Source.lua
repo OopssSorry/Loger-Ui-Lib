488f3b3a4162d62ba3424233a4848f8e00a80b03
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2022-09-25 12:20:32 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,50 +1,58 @@
-local Library = {}
-function Library:Frame(Text1, Font1, Color1)
-	print("123")
-	function dragify(Frame)
-	local dragToggle = nil
-	local dragInput = nil
-	local dragStart = nil
-	local dragPos = nil
-	local startPos = nil
-	function updateInput(input)
-		local Delta = input.Position - dragStart
-		local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-		game:GetService("TweenService"):Create(Frame, TweenInfo.new(.25), {Position = Position}):Play()
-	end
+print([[ 
+--
+--											
+-- Oopss Loger Ui - Loadet				
+-- Free UI logger made by Oopss-Sorry#2067 
+-- Discord: https://discord.gg/nAhmM6zEzy  
+--											
+--											
+]])
 
-	Frame.InputBegan:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-			dragToggle = true
-			dragStart = input.Position
-			startPos = Frame.Position
-			input.Changed:Connect(function()
-				if (input.UserInputState == Enum.UserInputState.End) then
-					dragToggle = false
-				end
-			end)
-		end
-	end)
-	Frame.InputChanged:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
-			dragInput = input
-		end
-	end)
-	game:GetService("UserInputService").InputChanged:Connect(function(input)
-		if (input == dragInput and dragToggle) then
-			updateInput(input)
-		end
-	end)
-end
-	if Text1 == nil then
-		Text1 = "Oopss Loger Ui"
+local Library = {}
+function Library:CreateFrame(label, font)
+	if font == nil then
+		font = Enum.Font.Arcade
 	end
-	if Color1 == nil then
-		Color1 = Color3.fromRGB(100, 100, 150)
+	local function dragify(Frame)
+		local dragToggle = nil
+		local dragInput = nil
+		local dragStart = nil
+		local dragPos = nil
+		local startPos = nil
+		local Delta = nil
+		local Position = nil
+		local function updateInput(input)
+			Delta = input.Position - dragStart
+			Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Frame, TweenInfo.new(.15), {Position = Position}):Play()
+		end
+
+		Frame.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+				dragToggle = true
+				dragStart = input.Position
+				startPos = Frame.Position
+				input.Changed:Connect(function()
+					if (input.UserInputState == Enum.UserInputState.End) then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+
+		Frame.InputChanged:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+				dragInput = input
+			end
+		end)
+
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if (input == dragInput and dragToggle) then
+				updateInput(input)
+			end
+		end)
 	end
-	if Font1 == nil then
-		Font1 = Enum.Font.Arcade
-	end
+	wait()
 	local _1 = Instance.new("ScreenGui")
 	local _1_1 = Instance.new("Frame")
 	local _1_1_1 = Instance.new("UICorner")
@@ -57,7 +65,7 @@ end
 	_1_1.Name = " "
 	_1_1.Parent = _1
 	_1_1.AnchorPoint = Vector2.new(0, 1)
-	_1_1.BackgroundColor3 = Color1
+	_1_1.BackgroundColor3 = Color3.fromRGB(100, 100, 150)
 	_1_1.Position = UDim2.new(0, 20, 1, -20)
 	_1_1.Size = UDim2.new(0, 300, 0, 300)
 	_1_1_1.Name = " "
@@ -84,25 +92,15 @@ end
 	_1_1_3.BorderSizePixel = 0
 	_1_1_3.Position = UDim2.new(0.5, 0, 0, 0)
 	_1_1_3.Size = UDim2.new(1, 0, 0.15, 0)
-	_1_1_3.Font = Font1
-	_1_1_3.Text = Text1
+	_1_1_3.Font = font
+	_1_1_3.Text = label
 	_1_1_3.TextColor3 = Color3.fromRGB(255, 255, 255)
 	_1_1_3.TextSize = 30.000
 	_1_1_3.TextWrapped = true
 	dragify(_1_1)
-	local msg = {}
 	local L = 0
-	function msg:Sent(Text2, Font2, Color2)
-		L = L + 1
-		if Text2 == nil then
-			Text2 = "Error"
-		end
-		if Color2 == nil then
-			Color2 = Color3.fromRGB(255, 255, 255)
-		end
-		if Font2 == nil then
-			Font2 = Enum.Font.Arcade
-		end
+	local MsgLibrary = {}
+	function MsgLibrary:Sent(text)
 		local NewMsg = Instance.new("TextLabel")
 		NewMsg.Name = L.." - msg"
 		NewMsg.RichText = true
@@ -110,13 +108,14 @@ end
 		NewMsg.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		NewMsg.BackgroundTransparency = 1.000
 		NewMsg.Size = UDim2.new(1, 0, 0, 25)
-		NewMsg.Text = Text2
-		NewMsg.TextColor3 = Color2
+		NewMsg.Text = text
+		NewMsg.TextColor3 = Color3.fromRGB(255, 255, 255)
 		NewMsg.TextScaled = false
 		NewMsg.TextSize = 30
 		NewMsg.TextWrapped = true
-		NewMsg.Font = Font2
+		NewMsg.Font = Enum.Font.Arcade
 		NewMsg.Parent = _1_1_2
+		L = L + 1
 	end
 	function msg:Clear()
 		L = 0
@@ -130,13 +129,6 @@ end
 	function Library.Remove()
 		_1:Destroy()
 	end
+	return MsgLibrary
 end
-print([[ 
---
---											
--- Oopss Loger Ui - Loadet				
--- Free UI logger made by Oopss-Sorry#2067 
--- Discord: https://discord.gg/nAhmM6zEzy  
---											
---											
-]])
+return Library
