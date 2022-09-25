378242ff958c7215c43d9872d5325a058701c947
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2022-09-25 12:22:41 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,8 +1,8 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/Loger-Ui-Lib/main/Source.lua"))()
-Lib:Create("Logger",Enum.Font.Arcade,nil)
-msg:Sent("Test1",Enum.Font.Arcade,nil)
+local msg = Lib:CreateFrame("Logger",Enum.Font.Arcade,nil)
+msg:SentMessage("Test1",Enum.Font.Arcade,nil)
 -----------------------------------
--- Lib.Create("Text",Enum.Font.Arcade,Color3.fromRGB(255, 255, 255)) -- Create frame
--- Lib.Remove() -- Remove Lib
--- msg:Sent("Text",Enum.Font.Arcade,Color3.fromRGB(255, 255, 255)) -- Sent message
--- msg:Clear() - clear all messages
+-- Lib.CreateFrame("Text",Enum.Font.Arcade,Color3.fromRGB(255, 255, 255)) -- Create frame
+-- Lib.RemoveFrame() -- Remove Lib
+-- msg:SentMessage("Text",Enum.Font.Arcade,Color3.fromRGB(255, 255, 255)) -- Sent message
+-- msg:ClearMessages() - clear all messages
