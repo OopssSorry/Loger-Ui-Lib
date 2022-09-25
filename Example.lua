af81bacb9f820cc55817d1fe0495da093b57c171
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2022-09-25 21:27:32 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,19 +1,23 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/Loger-Ui-Lib/main/Source.lua"))()
 local msg = Lib:CreateFrame("Logger",Enum.Font.Code)
-msg:SentMessage("Test1")
-wait(.1)
-msg:SentMessage("Test2")
-wait(3)
+msg:SentMessage("Test1-SentMessage")
+wait(1)
+msg:SentMessage("Test2-SentMessage")
+wait(1)
+msg:SentWarn("Test3-SentWarn")
+wait(1)
+msg:SentError("Test4-SentError")
+wait(1)
 msg:ClearMessages()
-msg:SentMessage("Test3")
-wait(.1)
-msg:SentMessage("Test4")
-wait(.1)
-msg:SentMessage("Test5")
-wait(5)
+msg:SentMessage("msg:ClearMessages()")
+wait(3)
+msg:SentMessage("Lib.RemoveFrame()")
+wait(1)
 Lib.RemoveFrame()
 -----------------------------------
 -- Lib.CreateFrame("Text",Enum.Font.Arcade) -- Create frame
 -- Lib.RemoveFrame() -- Remove Lib
 -- msg:SentMessage("Text") -- Sent message
+-- msg:SentWarn("Text") -- Sent warn
+-- msg:SentError("Text") -- Sent error
 -- msg:ClearMessages() - clear all messages
